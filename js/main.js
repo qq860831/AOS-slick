34f743049f225a9f4c953e22c98bd9acbe9fdcc3
HWChia <66512878+xmas21@users.noreply.github.com>
2020-06-21 15:44:43 +0800

    123

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-$('#banner').slick();
-$('#brand').slick();
+$('#banner').slick({
+    autoplay:true,
+    autoplaySpeed:1000,
+    arrows:false
+    // 箭頭去掉
+
+});
+$('#brand').slick({
+    slidesToShow:3,
+    autoplay:true,
+    autoplaySpeed:800,
+    dots:true,
+    arrows:false
+
+});
